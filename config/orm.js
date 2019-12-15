71b5256f0fb30b17edada2ff77a6d89abdc4862f
Omar Luna <mrebel62@gmail.com>
2019-12-15 12:48:25 -0600

    backend functions ready

---
+++ config/orm.js
@@ -0,0 +1,48 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// Object for all our SQL statement functions.
+var orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  create: function(table,vals, cb) {
+    var queryString = "INSERT INTO " + table;
+    queryString += "(burger_name, devoured) VALUES (?,?)" 
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  // An example of objColVals would be {name: panther, sleepy: true}
+  update: function(table, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET devoured = ?";
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Western Bacon Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Godzilla Thickburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', FALSE);
